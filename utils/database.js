49d3b0cd1b1d875116838105fff1e88960987bd4
Nourhen Arfaoui <nourarfeoui20@gmail.com>
2023-08-23 12:11:12 +0100

    modify database.js file

--- utils/database.js
+++ utils/database.js
@@ -1,25 +1,26 @@
-import mongoose from "mongoose";
-require('dotenv').config();
+import mongoose from 'mongoose';
 
-let isConnected = false ;
+let isConnected = false; // track the connection
 
-export const ConnectionToDB=async ()=>{
+export const ConnectionToDB = async () => {
+  mongoose.set('strictQuery', true);
 
-    mongoose.set('strictQuery',true)
-    if(isConnected){
-        console.log('already connected to database')
-        return;
-    }
-    try {
-       await mongoose.connect(process.env.MONGODB_URL,{
-            dbName: "script_s",
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        isConnected=true;
-        console.log('connected to database sucessfuly *\/*')
-    } catch (error) {
-        console.log(error)
-    }
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
